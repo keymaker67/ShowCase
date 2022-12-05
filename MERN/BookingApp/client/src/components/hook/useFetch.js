1291cc7229f19e8f53bcb2793df035bf7e210e24
keymaker67 <aminem67@gmail.com>
2022-12-05 09:35:54 +0330

    react ui

---
+++ MERN/BookingApp/client/src/components/hook/useFetch.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+export default function useFetch(url) {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const res = await axios.get(url);
+        setData(res.data);
+      } catch (error) {
+        setError(error);
+      }
+      setLoading(false);
+    };
+    fetchData();
+  }, [url]);
+
+  const reFetch = async () => {
+    try {
+      setLoading(true);
+      const res = await axios.get(url);
+      setData(res.data);
+    } catch (error) {
+      setError(error);
+    }
+    setLoading(false);
+  };
+
+  return { data, loading, error, reFetch };
+}
