3e1ce902cfa11be21113cfcc4427539cf25daf45
Amin Hoss <aminem67@gmail.com>
2024-02-27 18:43:59 +0330

    apps created

---
+++ Django/Instagram/instagram/instagram/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+    TokenObtainPairView,
+)
+from .local_settings import ADMIN_PATH
+
+urlpatterns = [
+    path(f'{ADMIN_PATH}/', admin.site.urls),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT,)
