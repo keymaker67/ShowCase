f7fb603e0064e4e94f47b281ea0057a00e7959d8
Amin Hoss <aminem67@gmail.com>
2024-02-28 09:32:00 +0330

    adding models
    adding content and user activity models

--- Django/Instagram/instagram/content/models.py
+++ Django/Instagram/instagram/content/models.py
@@ -1,2 +1,28 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
+from ..user.models import MyBaseModel
+
+User = get_user_model()
+
+
+# Create content models
+class PostModel(MyBaseModel):
+    user = models.OneToOneField(User, on_delete=models.PROTECT, blank=False, null=False, related_name='user_posts')
+    allow_comments = models.BooleanField(blank=True, default=True, verbose_name='Allow comments')
+    show_like = models.BooleanField(blank=True, default=True, verbose_name='Show like')
+    close_friends_only = models.BooleanField(blank=True, default=True, verbose_name='Close friends only')
+    mentioned_user = models.ManyToManyField(User, related_name='mentioned_users_post', blank=True,
+                                            verbose_name='Mentioned User')
+    caption = models.CharField(max_length=255, blank=True, verbose_name='Caption')
+
+    @property
+    def like_counts(self):
+        return self.post_likes.count()
+
+    class Meta:
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
+
+    def __str__(self):
+        return f'{str(self.user)} Created a post at {self.created_date}'
--- Django/Instagram/instagram/user_activity/models.py
+++ Django/Instagram/instagram/user_activity/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from rest_framework.exceptions import ValidationError
 
-# Create your models here.
+from ..user.models import MyBaseModel
+from ..content.models import PostModel
+
+User = get_user_model()
+
+
+# Create user activity models
+class CommentModel(MyBaseModel):
+    user = models.ForeignKey(User, on_delete=models.PROTECT, verbose_name='User',
+                             related_name='user_comments', blank=False, null=False)
+    post = models.ForeignKey(PostModel, on_delete=models.PROTECT, verbose_name='Post',
+                             related_name='post_comments', blank=False, null=False)
+    comment = models.TextField(verbose_name='Comment', blank=False, null=False)
+
+    @property
+    def like_count(self):
+        return self.comment_likes.count()
+
+    class Meta:
+        verbose_name = 'Comment'
+        verbose_name_plural = 'Comments'
+
+    def __str__(self):
+        return self.comment
+
+
+class LikeModel(MyBaseModel):
+    user = models.ForeignKey(User, on_delete=models.PROTECT, verbose_name='User',
+                             related_name='user_likes', blank=False, null=False)
+    post = models.ForeignKey(PostModel, on_delete=models.PROTECT, verbose_name='Post',
+                             related_name='post_likes', blank=False, null=False)
+    comment = models.ForeignKey(CommentModel, on_delete=models.PROTECT, verbose_name='Comment',
+                                related_name='comment_likes', blank=False, null=False)
+
+    def clean(self):
+        # Ensure that at least one of post or comment is set
+        if self.post_id is None and self.comment_id is None:
+            raise ValidationError('A like must be associated with either a post or a comment')
+
+    class Meta:
+        verbose_name = 'Like'
+        verbose_name_plural = 'Likes'
