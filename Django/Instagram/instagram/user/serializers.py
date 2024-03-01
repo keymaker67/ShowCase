7e2f94ff1a31abb23ba7cf7238fb803b22077acb
Amin Hoss <aminem67@gmail.com>
2024-03-01 03:35:03 +0330

    modified

--- Django/Instagram/instagram/user/serializers.py
+++ Django/Instagram/instagram/user/serializers.py
@@ -10,7 +10,7 @@ User = get_user_model()
 class UserSerializer(ModelSerializer):
     class Meta:
         model = User
-        fields = __all__
+        fields = '__all__'
 
 
 class UserProfileSerializer(ModelSerializer):
