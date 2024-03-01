f03f20125938fc1dd58f08cdd40387868ca7085a
Amin Hoss <aminem67@gmail.com>
2024-03-01 03:33:43 +0330

    urls.py created (content modified)

--- Django/Instagram/instagram/content/urls.py
+++ Django/Instagram/instagram/content/urls.py
@@ -1,8 +1,23 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 
-from .views import post_view
+from .views import (
+    post_view,
+    MediaViewSet,
+    MentionViewSet,
+    PostViewSet,
+    StoryViewSet
+)
+
+# Create routes
+content_router = routers.DefaultRouter()
+content_router.register('media', MediaViewSet, basename='media')
+content_router.register('mention', MentionViewSet, basename='mention')
+content_router.register('post', PostViewSet, basename='post')
+content_router.register('story', StoryViewSet, basename='story')
 
 # Create path
 urlpatterns = [
-    path('', post_view, name="home")
+    path('', post_view, name="home"),
+    path('', include(content_router.urls)),
 ]
--- Django/Instagram/instagram/instagram/urls.py
+++ Django/Instagram/instagram/instagram/urls.py
@@ -12,6 +12,11 @@ urlpatterns = [
     path(f'{ADMIN_PATH}/', admin.site.urls),
     path('', include('content.urls')),
     path('user/', include('user.urls')),
+    path('content/', include('content.urls')),
+    path('tag/', include('tag.urls')),
+    path('user_activity/', include('user_activity.urls')),
+    path('log/', include('log.urls')),
+    path('direct/', include('direct.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT,)
