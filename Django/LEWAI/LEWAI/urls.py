4ea9ebfb9789d223a3acb9a56d863e4fda86acca
Emin Güneş <aminem67@gmail.com>
2025-02-05 19:00:20 +0300

    main urls and views edited

--- Django/LEWAI/LEWAI/urls.py
+++ Django/LEWAI/LEWAI/urls.py
@@ -1,22 +1,23 @@
-"""
-URL configuration for LEWAI project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from .local_settings import ADMIN_URL
+from .views import home
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(f'{ADMIN_URL}/', admin.site.urls),
+    path('', home, name='home'),
+    path('', include('users.urls')),
+    path('accounts/', include('allauth.urls')),  # For login, logout, and registration
+    path('explore/', include('explore.urls')),
 ]
+
+urlpatterns += [
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
