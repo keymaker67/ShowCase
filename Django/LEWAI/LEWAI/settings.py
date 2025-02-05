73a217b8937c16f5a8e6d6c6e2ac59ae53072808
Emin Güneş <aminem67@gmail.com>
2025-02-05 19:43:52 +0300

    heroku settings

--- Django/LEWAI/LEWAI/settings.py
+++ Django/LEWAI/LEWAI/settings.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from .local_settings import *
 from datetime import timedelta
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -64,10 +65,7 @@ WSGI_APPLICATION = 'LEWAI.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default='sqlite///db.sqlite3')
 }
 
 # Password validation
